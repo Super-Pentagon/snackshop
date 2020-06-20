3090a92b9a25d6b56a066bb211e22fe610fab963
CodeHunter_qcy <1715464388@qq.com>
2020-06-20 18:44:16 +0800

    6.20整合swagger2

---
+++ service/service_user/src/main/java/com/qcy/userservice/BuyApplication.java
@@ -0,0 +1,15 @@
+package com.qcy.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CodeHunter_qcy
+ * @date 2020/6/20 - 18:29
+ */
+@SpringBootApplication
+public class BuyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuyApplication.class,args);
+    }
+}
---
+++ service/service_user/src/main/java/com/qcy/userservice/config/BuyConfig.java
@@ -0,0 +1,14 @@
+package com.qcy.userservice.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author CodeHunter_qcy
+ * @date 2020/6/20 - 18:31
+ */
+@Configuration
+@MapperScan("com.qcy.userservice.mapper")
+public class BuyConfig {
+
+}
